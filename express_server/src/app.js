92321937382e4f79c800c116a9646c1285dcc7e2
JGEsteves <jose.esteves.mail@gmail.com>
2020-04-18 18:14:49 +0200

    Added a barebone set up for the app

--- express_server/src/app.js
+++ express_server/src/app.js
@@ -1,1 +1,36 @@
-console.log("Hello darkness my old friend 2")
+console.log("*************************************")
+console.log("************ App started ************")
+console.log("*************************************")
+
+// Import modules:
+// Barebone js application framework
+const express = require('express')
+
+// A parser to process json data
+const bodyParser = require('body-parser')
+
+// Allows host/client to access
+const cors = require('cors')
+
+// For loging
+const morgan = require('morgan')
+
+// main app
+const app = express()
+// tells app to use morgan for log and 'combine' setting
+app.use(morgan('combined'))
+// use the bodyParser parser
+app.use(bodyParser.json())
+// set cors to make this accessible from client
+app.use(cors())
+
+// simple inpoint route for get request to a status inpoint.
+app.get('/status', (req, res) =>{
+    res.send({
+        message: 'Hello darkness my old friend'
+    })
+})
+
+// start server to handling requests on port
+app.listen(process.env.PORT || 8081)
+
